17b317f21666bce41bc1a0c019a4be08fea88ddd
97chos <97chos@naver.com>
2020-12-01 22:42:46 +0900

    Add side-bar and image mask effect

---
+++ MyMemory/MyMemory/SideBarVC.swift
@@ -0,0 +1,88 @@
+//
+//  SideBarVC.swift
+//  MyMemory
+//
+//  Created by sangho Cho on 2020/12/01.
+//
+
+import Foundation
+import UIKit
+
+class SideBarVC: UITableViewController {
+
+    // 목록 데이터 배열
+    let titles = ["새 글 작성하기","친구 새 글","달력으로 보기","공지사항","통계","계정 관리"]
+
+    // 아이콘 데이터 배열
+    let icons = [
+        UIImage(named: "icon01"),
+        UIImage(named: "icon02"),
+        UIImage(named: "icon03"),
+        UIImage(named: "icon04"),
+        UIImage(named: "icon05"),
+        UIImage(named: "icon06")
+    ]
+
+    let nameLabel = UILabel()
+    let emailLabel = UILabel()
+    let profileImage = UIImageView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 헤더뷰 생성
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 70))
+        headerView.backgroundColor = .brown
+        self.tableView.tableHeaderView = headerView
+
+        // 이름 레이블 속성 설정
+        self.nameLabel.frame = CGRect(x: 70, y: 15, width: 100, height: 30)
+        self.nameLabel.text = "조상호"
+        self.nameLabel.textColor = .white
+        self.nameLabel.font = .boldSystemFont(ofSize: 15)
+        self.nameLabel.backgroundColor = .clear
+
+        // 이메일 레이블 속성 설정
+        self.emailLabel.frame = CGRect(x: 70, y: 40, width: 100, height: 30)
+        self.emailLabel.text = "97chos@naver.com"
+        self.emailLabel.textColor = .white
+        self.emailLabel.font = .systemFont(ofSize: 11)
+        self.emailLabel.sizeToFit()
+        self.emailLabel.backgroundColor = .clear
+
+        // 기본 프로필 이미지 구현
+        let defaultProfile = UIImage(named: "account.jpg")
+        self.profileImage.image = defaultProfile
+        self.profileImage.frame = CGRect(x: 10, y: 10, width: 50, height: 50)
+
+        // 프로필 이미지 둥글게 만들기
+        self.profileImage.layer.cornerRadius = self.profileImage.frame.width / 2    // 반원 형태로 라운딩
+        self.profileImage.layer.borderWidth = 0                                     // 테두리 두께 0
+        self.profileImage.layer.masksToBounds = true                                // 마스크 효과로 가려진 이미지 반전 처리
+
+        headerView.addSubview(nameLabel)
+        headerView.addSubview(emailLabel)
+        headerView.addSubview(profileImage)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let id = "menucell"
+
+        // 재사용 큐에서 테이블 셀을 꺼내 온다. 없으면 새로 생성한다.
+        let cell = tableView.dequeueReusableCell(withIdentifier: id) ?? UITableViewCell(style: .default,
+                                                                                        reuseIdentifier: id)
+
+        // 타이틀과 이미지를 대입한다.
+        cell.textLabel?.text = titles[indexPath.row]
+        cell.imageView?.image = icons[indexPath.row]
+
+        // 폰트 설정
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
+
+        return cell
+    }
+}
