9ef750b6244dc1597764eb7f3347bf8bbdb31c88
97chos <97chos@naver.com>
2020-12-14 12:33:33 +0900

    메모 삭제 구현

--- MyMemory/MyMemory/MemoListVC.swift
+++ MyMemory/MyMemory/MemoListVC.swift
@@ -91,4 +91,20 @@ class MemoListVC: UITableViewController {
         vc.param = row
         self.navigationController?.pushViewController(vc, animated: true)
     }
+
+    // 편집 모드 지정
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
+        let data = self.appDelegate.memoList[indexPath.row]
+
+        // 코어 데이터에서 삭제한 다음, 배열 내 데이터 및 테이블 뷰 행을 차례로 삭제
+        if dao.delete(data.objectID!) {
+            self.appDelegate.memoList.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
 }
