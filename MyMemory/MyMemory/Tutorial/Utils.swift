cc4b8026a919d15bca03ea7806ec540f2bd8b267
97chos <97chos@naver.com>
2020-12-15 23:51:10 +0900

    키체인 기능 구현

--- MyMemory/MyMemory/Tutorial/Utils.swift
+++ MyMemory/MyMemory/Tutorial/Utils.swift
@@ -7,6 +7,55 @@
 
 import Foundation
 import UIKit
+import Security
+import Alamofire
+
+class TokenUnits {
+    // 키체인에 값을 저장하는 메소드
+    func save(_ service: String, account: String, value: String) {
+        let keyChainQuery: NSDictionary = [
+            kSecClass : kSecClassGenericPassword,
+            kSecAttrService : service,
+            kSecAttrAccount : account,
+            kSecValueData : value.data(using: .utf8, allowLossyConversion: false)!
+        ]
+
+        // 현재 저장되어 있는 값 삭제
+        SecItemDelete(keyChainQuery)
+
+        // 새로운 키 체인 아이템 등록
+        let status: OSStatus = SecItemAdd(keyChainQuery, nil)
+        assert(status == noErr, "토큰 값 저장에 실패했습니다.")
+        NSLog("status=\(status)")
+    }
+
+    // 키 체인에 저장된 값을 읽어오는 메소드
+    func load(_ service: String, account: String, value: String) -> String? {
+        // 1. 키 테인 쿼리 정의
+        let keyChainQuery: NSDictionary = [
+            kSecClass : kSecClassGenericPassword,
+            kSecAttrService : service,
+            kSecAttrAccount : account,
+            kSecReturnData : kCFBooleanTrue,    // CFDataRef
+            kSecMatchLimit : kSecMatchLimitOne
+        ]
+
+        // 2. 키 체인에 저장된 값 읽어오기
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(keyChainQuery, &dataTypeRef)
+
+        // 3. 처리 결과가 성공이라면 읽어온 값을 Data 타입으로 변환하고, 다시 String 타입으로 변환
+        // errSecSuccess = 성공을 의미하는 OSStatus 타입 값
+        if (status == errSecSuccess) {
+            let retrievedData = dataTypeRef as! Data
+            let value = String(data: retrievedData, encoding: String.Encoding.utf8)
+            return value
+        } else {    // 4. 처리 결과가 실패라면 nil을 반환
+            print("Nothing was retrieved from the keychain. Status code \(status)")
+            return nil
+        }
+    }
+}
 
 extension UIViewController {
     var tutorialSB: UIStoryboard {
